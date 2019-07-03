cf505f4faecc582380315de09c2a1d54a2f7cc58
feng <fengtony686@gmail.com>
2019-07-03 13:51:09 +0800

    .

--- python/threading.py
+++ python/threading.py
@@ -1,17 +1,13 @@
-import time, threading
+balance = 0
+lock = threading.Lock()
 
-# 新线程执行的代码:
-def loop():
-    print('thread %s is running...' % threading.current_thread().name)
-    n = 0
-    while n < 5:
-        n = n + 1
-        print('thread %s >>> %s' % (threading.current_thread().name, n))
-        time.sleep(1)
-    print('thread %s ended.' % threading.current_thread().name)
-
-print('thread %s is running...' % threading.current_thread().name)
-t = threading.Thread(target=loop, name='LoopThread')
-t.start()
-t.join()
-print('thread %s ended.' % threading.current_thread().name)
+def run_thread(n):
+    for i in range(100000):
+        # 先要获取锁:
+        lock.acquire()
+        try:
+            # 放心地改吧:
+            change_it(n)
+        finally:
+            # 改完了一定要释放锁:
+            lock.release()
